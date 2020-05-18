60b597d604a29485090ec463c419e0cbf765b15d
Jose Carlos Cardenas Martinez <josecm@icloud.com>
2020-05-17 22:17:47 -0500

    Primera parte de Ravenous

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
 import './App.css';
 
+class App extends React.Component {
+  render() { 
+    return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+    );
+  }
+}
+ 
+export default App;
+
+/* Esta chingadera ya venia y no es un componente
 function App() {
   return (
     <div className="App">
@@ -22,5 +38,5 @@ function App() {
     </div>
   );
 }
+*/
 
-export default App;
